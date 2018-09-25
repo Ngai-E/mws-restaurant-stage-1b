4ccf42186e23d0698e9af9bdb61f21e97b2ba3a0
Ngai Elizabeth Asobi <asobingai@gmail.com>
2018-09-24 21:27:49 -0700

    change the server to listen to port 1337

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,15 +8,18 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    /*const port = 8000 // Change this to your server port
+    return `http://localhost:${port}/data/restaurants.json`;*/
+
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;    
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
+    /*let xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
@@ -28,7 +31,19 @@ class DBHelper {
         callback(error, null);
       }
     };
-    xhr.send();
+    xhr.send();*/
+
+    return fetch(DBHelper.DATABASE_URL)
+          .then(response => {
+            return response.json();
+          })
+          .then(restaurants => {
+            callback(null, restaurants);
+          })
+          .catch(e => {
+            callback(`Request failed. Returned status of ${e}`, null);
+          })
+
   }
 
   /**
