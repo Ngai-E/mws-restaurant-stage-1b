44d2381594e25bc8e5cdd416f0a098492cfdaa16
Ngai Elizabeth Asobi <asobingai@gmail.com>
2018-10-03 14:22:56 -0700

    add Jake Archibald's IndexedDB Promised library

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,6 +6,7 @@
  					'css/responsive.css',
  					'js/dbhelper.js',
  					'js/main.js',
+          'js/idb.js',
  					'js/restaurant_info.js',
 					'https:unpkg.com/leaflet@1.3.1/dist/leaflet.js',
 					'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
